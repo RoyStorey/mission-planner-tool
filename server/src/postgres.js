1edeff9ef0fbe4ce6d10fcdf50c52a2b1b1f22c7
Roy Storey <imlejiii@gmail.com>
2023-09-13 17:24:40 +0000

    env for postgres

--- server/src/postgres.js
+++ server/src/postgres.js
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 
 const postgreSQLClient = new Pool({
   user: process.env.PGUSER,
-  host: '172.16.220.118',
+  host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
