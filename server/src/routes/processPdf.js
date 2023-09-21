b27a9ac5a62d6b9cf64fb71e72c02dbd16a14470
RoyStorey <imlejiii@gmail.com>
2023-09-21 12:23:40 +0000

    not a full fix

--- server/src/routes/processPdf.js
+++ server/src/routes/processPdf.js
@@ -79,10 +79,14 @@ function render_page(pageData) {
         pageStarted = false;
       }
 
+      let airportCodeRegex = /^[A-Z]{4}$/;
+
       if (pageStarted) {
         if (
+          currentString.split(" ").length === 1 && 
           currentString.trim().length === 4 &&
           !rowStarted &&
+          airportCodeRegex.test(currentString) &&
           currentString === currentString.toUpperCase() &&
           !["NSTR"].includes(currentString)
         ) {
@@ -94,36 +98,10 @@ function render_page(pageData) {
           switch (currentCol) {
             case 1:
               if (previousString === "DH") currentLeg.DH = previousString;
-
               let airportCodeRegex = /^[A-Z]{4}$/;
-              let concattedAirportArray = currentString.split(" ").map(item=>item.trim())
-              console.log(currentString, concattedAirportArray)
-
-              if(concattedAirportArray.length == 1 && airportCodeRegex.test(concattedAirportArray[0])){
-                currentLeg.from = concattedAirportArray[0];
-                currentCol += 1;
-                console.log('1')
-                break
-              }
-              else if(concattedAirportArray.length == 2 && airportCodeRegex.test(concattedAirportArray[0])){
-                currentLeg.from = concattedAirportArray[0];
-                currentLeg.ddzulu = concattedAirportArray[1]
-                currentCol += 2;
-                console.log('2')
-                break
-              }
-              else if(concattedAirportArray.length == 3 && airportCodeRegex.test(concattedAirportArray[1])){
-                currentLeg.from = concattedAirportArray[1];
-                currentLeg.ddzulu = concattedAirportArray[2]
-                currentCol += 2;
-                console.log('3')
-                break
-              }
-              else{
-                console.log('4')
-                break
-              }
-
+              currentLeg.ddzulu = currentString;
+              currentCol += 1;
+              break;
             case 2:
               currentLeg.ddzulu = currentString;
               currentCol += 1;
