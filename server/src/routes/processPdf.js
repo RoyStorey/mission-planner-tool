93b2b783f4ce6f0fa6fc57ec593247d03450467a
RoyStorey <imlejiii@gmail.com>
2023-09-21 13:17:15 +0000

    god is angry

--- server/src/routes/processPdf.js
+++ server/src/routes/processPdf.js
@@ -83,19 +83,62 @@ function render_page(pageData) {
       let concattedArray = currentString.split(" ")
 
       if (pageStarted) {
-        if (
-          concattedArray.length === 1 && 
-          concattedArray[0].trim().length === 4 &&
-          !rowStarted &&
-          airportCodeRegex.test(concattedArray[0].trim()) &&
-          concattedArray[0] === concattedArray[0].toUpperCase() &&
-          !["NSTR"].includes(concattedArray[0])
-        ) {
-          console.log(concattedArray[0])
-          currentLeg = {};
-          rowStarted = true;
-          currentCol += 1;
+        // if (
+        //   concattedArray.length === 1 &&
+        //   concattedArray[0].trim().length === 4 &&
+        //   !rowStarted &&
+        //   airportCodeRegex.test(concattedArray[0].trim()) &&
+        //   concattedArray[0] === concattedArray[0].toUpperCase() &&
+        //   !["NSTR"].includes(concattedArray[0])
+        // ) {
+        //   console.log(concattedArray[0])
+        //   currentLeg = {};
+        //   rowStarted = true;
+        //   currentCol += 1;
+        // }
+
+        if(concattedArray.length === 1){
+          if(
+            concattedArray[0].trim().length === 4 &&
+            !rowStarted &&
+            airportCodeRegex.test(concattedArray[0].trim()) &&
+            concattedArray[0] === concattedArray[0].toUpperCase() &&
+            !["NSTR"].includes(concattedArray[0])){
+              console.log(concattedArray[0])
+              currentLeg = {};
+              rowStarted = true;
+              currentCol += 1;
+          }
         }
+
+        if(concattedArray.length === 2){
+          if(
+            concattedArray[0].trim().length === 4 &&
+            !rowStarted &&
+            airportCodeRegex.test(concattedArray[0].trim()) &&
+            concattedArray[0] === concattedArray[0].toUpperCase() &&
+            !["NSTR"].includes(concattedArray[0])){
+              console.log(concattedArray[0])
+              currentLeg = {};
+              rowStarted = true;
+              currentCol += 2;
+          }
+        }
+
+        if(concattedArray.length === 3){
+          if(
+            concattedArray[1].trim().length === 4 &&
+            !rowStarted &&
+            airportCodeRegex.test(concattedArray[1].trim()) &&
+            concattedArray[1] === concattedArray[1].toUpperCase() &&
+            !["NSTR"].includes(concattedArray[1])){
+              console.log(concattedArray[1])
+              currentLeg = {};
+              rowStarted = true;
+              currentCol += 2;
+          }
+        }
+
         if (rowStarted) {
           switch (currentCol) {
             case 1:
