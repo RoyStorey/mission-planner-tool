6a822cbb56b7109eb130095d9d4cf0962b516aae
Brandon Freeman <bfreeman@mdt-clear-04.samfoxcyber.space>
2023-09-22 19:01:47 +0000

    reordered upcoming mission panel

--- client/src/components/UpcomingMissions.vue
+++ client/src/components/UpcomingMissions.vue
@@ -5,6 +5,7 @@
       :columns="columns"
       :data="missions"
       :pagination="pagination"
+      shrink-text
     />
     <template #action>
       <router-link :to="{ path: '/missions' }" style="text-decoration: none">
