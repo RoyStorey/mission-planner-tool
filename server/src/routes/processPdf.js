020fea4eb9cfc639614e089536fd26d90364f0cb
Brandon Freeman <bfreeman@mdt-clear-04.samfoxcyber.space>
2023-09-22 14:55:33 +0000

    lol nope

--- server/src/routes/processPdf.js
+++ server/src/routes/processPdf.js
@@ -230,8 +230,8 @@ const options = {
 const processPDF = async (req, res) => {
   try {
     let dataBuffer = fs.readFileSync(req.file.path);
+    console.log(req.file);
     const data = await pdf(dataBuffer, options);
-    fs.unlink(req.file.path);
     return res.json(listOfMissions);
   } catch (error) {
     res.sendStatus(500);
