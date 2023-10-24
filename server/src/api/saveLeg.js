3a5b6446bf0cc3dccc54b952487f3639d2d5887d
Roy Storey <imlejiii@gmail.com>
2023-10-24 14:02:42 +0000

    commented out existing leg stuff

--- server/src/api/saveLeg.js
+++ server/src/api/saveLeg.js
@@ -20,64 +20,64 @@ const saveLeg = async ({
 }) => {
   const client = await postgreSQLClient.connect();
   try {
-    const findExistingLeg = `
-    SELECT *
-    FROM legs
-    WHERE "from" = $1
-    AND "to" = $2
-    AND airport = $3
-    AND country = $4
-    AND mission_number = $5
-    `;
+    // const findExistingLeg = `
+    // SELECT *
+    // FROM legs
+    // WHERE "from" = $1
+    // AND "to" = $2
+    // AND airport = $3
+    // AND country = $4
+    // AND mission_number = $5
+    // `;
 
-    const { rows: existingLegs } = await client.query(findExistingLeg, [
-      from,
-      to,
-      airport,
-      country,
-      missionNumber,
-    ]);
+    // const { rows: existingLegs } = await client.query(findExistingLeg, [
+    //   from,
+    //   to,
+    //   airport,
+    //   country,
+    //   missionNumber,
+    // ]);
 
-    if (existingLegs.length > 0) {
-      const existingId = existingLegs[0].id;
-      const updateLeg = `
-      UPDATE legs
-      SET dh = $1,
-      "from" = $2,
-      dd_zulu = $3,
-      "to" = $4,
-      airport = $5,
-      country = $6,
-      arrival_date = $7,
-      ete = $8,
-      duty_day = $9,
-      gnd_time = $10,
-      mission_number = $11,
-      dvcode = $12
-      WHERE id = $13
-      RETURNING id;
-      `;
+    // if (existingLegs.length > 0) {
+    //   const existingId = existingLegs[0].id;
+    //   const updateLeg = `
+    //   UPDATE legs
+    //   SET dh = $1,
+    //   "from" = $2,
+    //   dd_zulu = $3,
+    //   "to" = $4,
+    //   airport = $5,
+    //   country = $6,
+    //   arrival_date = $7,
+    //   ete = $8,
+    //   duty_day = $9,
+    //   gnd_time = $10,
+    //   mission_number = $11,
+    //   dvcode = $12
+    //   WHERE id = $13
+    //   RETURNING id;
+    //   `;
 
-      const { rows } = await client.query(updateLeg, [
-        DH,
-        from,
-        ddzulu,
-        to,
-        airport,
-        country,
-        arrDate,
-        ete,
-        dutyDay,
-        groundTime,
-        missionNumber,
-        dvcode,
-        existingId,
-      ]);
+    //   const { rows } = await client.query(updateLeg, [
+    //     DH,
+    //     from,
+    //     ddzulu,
+    //     to,
+    //     airport,
+    //     country,
+    //     arrDate,
+    //     ete,
+    //     dutyDay,
+    //     groundTime,
+    //     missionNumber,
+    //     dvcode,
+    //     existingId,
+    //   ]);
 
-      client.release();
-      return rows[0];
-    } else {
-      const query = `
+    //   client.release();
+    //   return rows[0];
+    // } else {
+    const query = `
     INSERT INTO legs
       (dh,
       "from",
@@ -93,25 +93,25 @@ const saveLeg = async ({
       dvcode) 
       values($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12)
     RETURNING id;
-  `;
+    `;
 
-      const { rows } = await client.query(query, [
-        DH,
-        from,
-        ddzulu,
-        to,
-        airport,
-        country,
-        arrDate,
-        ete,
-        dutyDay,
-        groundTime,
-        missionNumber,
-        dvcode,
-      ]);
-      client.release();
-      return rows[0];
-    }
+    const { rows } = await client.query(query, [
+      DH,
+      from,
+      ddzulu,
+      to,
+      airport,
+      country,
+      arrDate,
+      ete,
+      dutyDay,
+      groundTime,
+      missionNumber,
+      dvcode,
+    ]);
+    client.release();
+    return rows[0];
+    // }
   } catch (error) {
     console.log(error);
     client.release();
